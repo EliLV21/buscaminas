ddc5d013be145c76fa83961f71c2c8cf6318bc9b
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-04-05 12:08:18 -0500

    terminado

--- buscaminas.js
+++ buscaminas.js
@@ -1,7 +1,24 @@
 var minas = document.getElementsByClassName("celda");
 var color = document.getElementsByClassName("empty");
 var reiniciarTodo = document.getElementsByTagName("input");
+var numero1 = document.getElementsByClassName("num 1");
+var numero2 = document.getElementsByClassName("num 2");
+var numero3 = document.getElementsByClassName("num 3");
+var facilito = document.getElementById("nivel1");
+var mediano = document.getElementById("nivel2");
+var dificil = document.getElementById("nivel3");
 
+for(var i=0;i<numero1.length;i++){
+  numero1[i].addEventListener("click",num1);
+};
+
+for(var i=0;i<numero2.length;i++){
+  numero2[i].addEventListener("click",num2);
+};
+
+for(var i=0;i<numero3.length;i++){
+  numero3[i].addEventListener("click",num3);
+};
 
 for(var i=0;i<color.length;i++){
   color[i].addEventListener("click",colors);
@@ -12,11 +29,23 @@ for(var i=0;i<minas.length;i++){
 };
 
 function bombas(){
-  alert("PERDISTEEE!!")
-  this.style.backgroundImage = "url('http://www.dafont.com/img/df_avatar.png')"
-  this.style.backgroundImage.size = "10px"
+  this.style.backgroundImage = "url('https://1.bp.blogspot.com/-ubqxxZa5O94/V6hhPFxah8I/AAAAAAABHl8/n3uUT4-rdOgBbv04WPF-Brf7sj0S2nNuACLcB/s70-c/Tsar%2BBomba.png')"
   reiniciar.disabled=true;
-  detener();
+  alert("PERDISTEEE!!")
+  //location.reload()
+  detener(event);
+};
+
+function num1(){
+  this.style.backgroundImage = "url('https://www.commbank.com.au/content/dam/commbank/assets/personal/credit-cards/campaigns/commbank-checkout-1.png')"
+};
+
+function num2(){
+  this.style.backgroundImage = "url('https://www.commbank.com.au/content/dam/commbank/assets/personal/credit-cards/campaigns/commbank-checkout-2.png')"
+};
+
+function num3(){
+  this.style.backgroundImage = "url('https://www.commbank.com.au/content/dam/commbank/assets/personal/credit-cards/campaigns/commbank-checkout-3.png')"
 };
 
 function reiniciar (){
@@ -29,10 +58,10 @@ function reiniciar (){
 };
 
 function colors(){
-  this.style.backgroundColor = "#F10226"
-  detener()
+  this.style.backgroundColor = "#C64371"
+  detener(event)
   };
 
-function detener(){
+function detener(event){
   event.stopPropagation();
 };
